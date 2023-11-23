0e6df28783d4c4a08d274f1cb5e81bb25042ec35
agastya gaur <agastyagaur17@gmail.com>
2023-11-23 23:22:06 +0530

    add userschema pre-hook with function

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose,{Schema} from "mongoose";
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
@@ -16,5 +17,36 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// password hashing before saving
+userSchema.pre("save", async function (next) {
+ 
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+})
+
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.methods.getSignedToken = function () {
+  return jwt.sign({ id: this._id  }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  })};
+
+  userSchema.methods.getRefreshToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET_REFRESH, {
+      expiresIn: process.env.JWT_EXPIRE_REFRESH,
+    });
+  };
+
+
+
  const User = mongoose.model("User", userSchema);
-    export default User;
+ export default User;
