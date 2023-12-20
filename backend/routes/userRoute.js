9c52701414325e6d22d872f5cfa6ea0d23f26548
agastya gaur <agastyagaur17@gmail.com>
2023-12-20 14:19:19 +0530

    Add refresh token route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -17,4 +17,5 @@ router.route("/register").post(
     router.route("/login").post(loginUser)
     router.route("/logout").post(verifyJWT,logOut)
     router.route("/refresh-token").post(refreshAccessToken)
+    
 export default router;
